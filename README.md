24babc2ff11fb72cef350b1b382c20e66ffe4f60
sherry <sherry.kisilu@studebnt.moringaschool.com>
2022-09-06 16:58:08 +0300

    well done styling

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # first-moringa-project
+project name;weather in a city
+author:Sherry
+It tells ypou the weather in different areas
--- script.js
+++ script.js
@@ -4,38 +4,32 @@ const description=document.getElementById("description")
 const humidity=document.getElementById("humidity")
 const wind=document.getElementById("wind")
 const temperature=document.getElementById("temp")
-
-
-
-
+const weather=document.getElementById("weather")
+let city=document.getElementById("search-bar");
 document.addEventListener("DOMContentLoaded",function(){
-    searchButton.addEventListener("click",function(){
-        let city=document.getElementById("search-bar").value;
-        console.log(city);
-        const apiKey="06d12ce8edb650299e20a27648cccec7";
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-        .then((response)=>response.json())
-        .then((data)=>displayWeather(data))
-    })
+    searchButton.addEventListener("click",fetchData)
 });
-
+function fetchData(){
+    let searchCity=city.value;
+    const apiKey="06d12ce8edb650299e20a27648cccec7";
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`)
+    .then((response)=>response.json())
+    .then((data)=>displayWeather(data))   
+    console.log (city.value);
+}
 function  displayWeather(data){
     console.log(data)
+    console.log(weather);
+     weather.style.visibility='visible';
 place.textContent=` Weather in ${data.name}`
-description.textContent=data.weather[0].description;
-humidity.textContent=data.main.humidity;
-wind.textContent=data.wind.speed;
-temperature.textContent= data.main.temp;
+description.textContent=`Description:${data.weather[0].description}`;
+humidity.textContent=`Humidity:${data.main.humidity}`;
+wind.textContent=`Wind:${data.wind.speed}`;
+temperature.textContent= `Temperature:${data.main.temp}`;
 } 
 
-// document.querySelector(".search button").addEventListener("click",function(){
-//     weather.search();
-// })
-// document.querySelector(".search-bar").addEventListener("keyup",function(event){
-//     if(event.key=='Enter'){
-//         weather.search();
-//     }
-// });
-// document.querySelector(".search button").addEventListener("mousehover",function(){
-//     return search();
-// })
+document.addEventListener("keydown",function(event){
+    if(event.key=='Enter'){
+        fetchData();
+    }
+});
